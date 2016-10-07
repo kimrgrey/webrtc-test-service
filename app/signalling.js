c256cc24ced4ede75333f8ec2c6c9bc36bb90984
Dmitry Toropchin <dmitry.toropchin@gmail.com>
2016-10-07 21:32:05 +0300

    separated request for user id

--- app/signalling.js
+++ app/signalling.js
@@ -24,32 +24,35 @@ const onIoConnection = (client) => {
 
   clients[client.id] = client;
 
+  client.on('register',   (params) => onClientRegister(client, params));
   client.on('join',       (params) => onClientJoin(client, params));
   client.on('leave',      (params) => onClientLeave(client, params));
   client.on('webrtc',     (params) => onClientWebRTC(client, params));
   client.on('disconnect', (params) => onClientDisconnect(client, params));
+};
 
+const onClientRegister = (client, params) => {
   __emit(client, 'registered', { id: client.id });
 };
 
 const onClientJoin = (client, params) => {
-  const { room } = JSON.parse(params);
+  const { id, name } = JSON.parse(params);
 
   if (client.room !== undefined) {
-      __broadcast(client, 'left', { user_id: client.id });
+      __broadcast(client, 'left', { id: client.id });
       client.leave(client.room);
   }
 
-  client.room = room;
-  client.join(room);
+  client.room = id;
+  client.join(client.room);
 
-  __broadcast(client, 'joined', { user_id: client.id });
-  __emit(client, 'members', { ids: clients.keys() });
+  __broadcast(client, 'joined', { id: client.id });
+  __emit(client, 'members', { ids: Object.keys(clients) });
 };
 
 const onClientLeave = (client, params) => {
   if (client.room !== undefined) {
-    __broadcast(client, 'left', { user_id: client.id });
+    __broadcast(client, 'left', { id: client.id });
     client.leave(client.room);
   }
 
@@ -69,7 +72,7 @@ const onClientWebRTC = (client, params) => {
 
 const onClientDisconnect = (client, params) => {
   if (client.room !== undefined) {
-    __broadcast(client, 'left', { user_id: client.id });
+    __broadcast(client, 'left', { id: client.id });
     client.leave(client.room);
   }
 
