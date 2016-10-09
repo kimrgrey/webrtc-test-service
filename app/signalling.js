b761cc61665b08979ee4aee7bb7053bdbe34b7ed
Dmitry Toropchin <dmitry.toropchin@gmail.com>
2016-10-10 01:06:32 +0300

    dependencies update, socket broadcast workaround

--- app/signalling.js
+++ app/signalling.js
@@ -6,11 +6,17 @@ const clients = {};
 const __generateClientId = () => randomstring.generate(8);
 
 const __emit = (receiver, message, params) => {
-  receiver.emit(message, JSON.stringify(params));
+  receiver.emit(message, params);
 };
 
 const __broadcast = (sender, message, params) => {
-  sender.broadcast.to(sender.room).emit(message, JSON.stringify(params));
+  // FIXME: not working broadcast.to(...)
+  // sender.broadcast.to(sender.room).emit(message, params);
+
+  Object.keys(clients)
+    .map((id) => (clients[id]))
+    .filter((client) => (client !== sender && client.room === sender.room))
+    .forEach((client) => client.emit(message, params));
 };
 
 const initSocketIo = (server) => {
@@ -24,22 +30,24 @@ const onIoConnection = (client) => {
 
   clients[client.id] = client;
 
-  client.on('register',   (params) => onClientRegister(client, params));
-  client.on('join',       (params) => onClientJoin(client, params));
-  client.on('leave',      (params) => onClientLeave(client, params));
-  client.on('webrtc',     (params) => onClientWebRTC(client, params));
-  client.on('disconnect', (params) => onClientDisconnect(client, params));
+  client.on('register',      (params) => onClientRegister(client, params));
+  client.on('join',          (params) => onClientJoin(client, params));
+  client.on('leave',         (params) => onClientLeave(client, params));
+  client.on('webrtc',        (params) => onClientWebRTC(client, params));
+  client.on('disconnecting', (params) => onClientDisconnecting(client, params));
+  client.on('disconnect',    (params) => onClientDisconnect(client, params));
 };
 
 const onClientRegister = (client, params) => {
-  __emit(client, 'registered', { id: client.id });
+  console.log('register from', client.id);
+  __emit(client, 'registered', JSON.stringify({ id: client.id }));
 };
 
 const onClientJoin = (client, params) => {
   const { id, name } = JSON.parse(params);
 
   if (client.room !== undefined) {
-      __broadcast(client, 'left', { id: client.id });
+      __broadcast(client, 'left', JSON.stringify({ id: client.id }));
       client.leave(client.room);
   }
 
@@ -47,15 +55,18 @@ const onClientJoin = (client, params) => {
     .filter((k) => (clients[k].room === id));
 
   client.room = id;
-  client.join(client.room);
+  client.join(client.room, (err) => {
+    console.log('join from', client.id, err);
 
-  __broadcast(client, 'joined', { id: client.id });
-  __emit(client, 'members', members);
+    __broadcast(client, 'joined', JSON.stringify({ id: client.id }));
+    __emit(client, 'members', JSON.stringify(members));
+  });
 };
 
 const onClientLeave = (client, params) => {
+  console.log('leave from', client.id);
   if (client.room !== undefined) {
-    __broadcast(client, 'left', { id: client.id });
+    __broadcast(client, 'left', JSON.stringify({ id: client.id }));
     client.leave(client.room);
   }
 
@@ -73,12 +84,14 @@ const onClientWebRTC = (client, params) => {
   }
 };
 
-const onClientDisconnect = (client, params) => {
+const onClientDisconnecting = (client, params) => {
   if (client.room !== undefined) {
-    __broadcast(client, 'left', { id: client.id });
+    __broadcast(client, 'left', JSON.stringify({ id: client.id }));
     client.leave(client.room);
   }
+};
 
+const onClientDisconnect = (client, params) => {
   delete clients[client.id];
 };
 
