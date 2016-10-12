cb4605dfff01243e4197181cd0a82951988ede15
Dmitry Toropchin <dmitry.toropchin@gmail.com>
2016-10-12 09:35:17 +0300

    removed https support

--- app/index.js
+++ app/index.js
@@ -1,6 +1,4 @@
 const http = require('http');
-const https = require('https');
-const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -13,19 +11,8 @@ const roomRouter = require('./routers/room');
 
 const roomStorage = require('./models/rooms');
 
-const config = {
-  certKeyPath: 'keys/webrtc.key',
-  certCrt: 'keys/webrtc.crt',
-};
-
 const application = express();
-// const server = http.createServer(application);
-const server = https.createServer({
-    key: fs.readFileSync(config.certKeyPath),
-    cert: fs.readFileSync(config.certCrt),
-    requestCert: true,
-    rejectUnauthorized: false
- }, application)
+const server = http.createServer(application);
 
 signalling.init(server);
 
