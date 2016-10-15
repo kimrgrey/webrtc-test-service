364cb887119e442fa12550eff6e9f246b3529bc7
Dmitry Toropchin <dmitry.toropchin@gmail.com>
2016-10-15 19:30:34 +0300

    configuration with nconf

--- app/index.js
+++ app/index.js
@@ -5,10 +5,10 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bluebird = require('bluebird');
 
+
+const config = require('./config');
 const logger = require('./utils/logger');
-
 const signalling = require('./signalling');
-
 const roomRouter = require('./routers/room');
 
 const application = express();
@@ -16,13 +16,16 @@ const server = http.createServer(application);
 
 mongoose.Promise = bluebird;
 
-mongoose.connect('mongodb://127.0.0.1:27017/webrtc_test_deb');
+mongoose.connect(config.get('db:connection') + '/' + config.get('db:name'),
+                 config.get('db:options'));
 
 mongoose.connection.on('open', () => {
-  console.log('database connected');
+  console.log('database connected on',
+              config.get('db:connection') + '/' + config.get('db:name'));
 });
 mongoose.connection.on('error', () => {
-  console.log('database connection error');
+  console.log('database connection error on',
+              config.get('db:connection') + '/' + config.get('db:name'));
 });
 
 signalling.init(server);
